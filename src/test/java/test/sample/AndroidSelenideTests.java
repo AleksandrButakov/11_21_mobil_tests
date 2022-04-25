039155b7bd615350f8c1de9f81f0ef982d7d381a
Aleksandr Butakov <anbngm@gmail.com>
2022-04-25 19:44:52 +0500

    Added sample android tests

---
+++ src/test/java/test/sample/AndroidSelenideTests.java
@@ -0,0 +1,46 @@
+package test.sample;
+
+import com.codeborne.selenide.Selenide;
+import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AndroidSelenideTests extends TestBase {
+
+    /*
+       Тест находящийся в классе AndroidSampleTests написанный на Selenium перепишем на Selenoid
+     */
+    @Test
+    @DisplayName("My tests")
+    void searchTest() {
+    /*
+        AndroidElement searchElement = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.elementToBeClickable(
+                        MobileBy.AccessibilityId("Search Wikipedia")));
+        searchElement.click();
+     */
+        $(MobileBy.AccessibilityId("Search Wikipedia")).click();
+
+    /*
+        AndroidElement insertTextElement = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.elementToBeClickable(
+                        MobileBy.id("org.wikipedia.alpha:id/search_src_text")));
+        insertTextElement.sendKeys("BrowserStack");
+     */
+        $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
+
+    /*
+        Thread.sleep(5000);
+        List<AndroidElement> allProductsName = driver.findElementsByClassName(
+                "android.widget.TextView");
+        assert (allProductsName.size() > 0);
+     */
+        $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0));
+
+    }
+
+}
---
+++ src/test/java/test/sample/TestBase.java
@@ -0,0 +1,25 @@
+package test.sample;
+
+import com.codeborne.selenide.Configuration;
+import helpers.BrowserstackMobileDriver;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBase {
+
+    @BeforeAll
+    public static void setup() {
+        Configuration.browser = BrowserstackMobileDriver.class.getName();
+        Configuration.startMaximized = false;
+        Configuration.browserSize = null;
+    }
+
+    @BeforeEach
+    public void startDriver() {
+        // open необходим, такая особенность работы Selenide. Можно вынести в @BeforeEach
+        open();
+    }
+
+}
