4bea6c4608cf106f72d1cd57d10cd1df7e271ed7
Aleksandr Butakov <anbngm@gmail.com>
2022-04-26 23:24:05 +0500

    Added allure listener

--- src/test/java/test/AndroidSelenideTests.java
+++ src/test/java/test/AndroidSelenideTests.java
@@ -2,6 +2,7 @@ package test;
 
 import io.appium.java_client.MobileBy;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import test.TestBase;
 
@@ -10,8 +11,8 @@ import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
+@Tag("selenide")
 public class AndroidSelenideTests extends TestBase {
-
     /*
        Тест находящийся в классе AndroidSampleTests написанный на Selenium перепишем на Selenoid
      */
